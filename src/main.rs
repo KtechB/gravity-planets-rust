4e0f71739e2e6c92b8a1d1c00c918881abecb8ee
ktechb <k_techevo@yahoo.co.jp>
2022-05-15 17:04:55 +0900

    fix planets m and add number limit

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,20 @@ use vec3::Vec3;
 fn window_conf() -> Conf {
     Conf {
         window_title: "Window name".to_owned(),
+        // window_height: 800,
+        // window_width: 1000,
         fullscreen: true,
         ..Default::default()
     }
 }
+
+const PLANETS_MAX_NUM: usize = 12;
 #[macroquad::main(window_conf)]
 async fn main() {
     let width: f64 = screen_width() as f64;
     let height: f64 = screen_height() as f64;
     let p1 = Planet::new(
-        5.0,
+        2.0,
         Vec3::new(width / 2.0, 1.0, 0.0),
         Vec3::new(50.0, 0.0, 0.0),
     );
@@ -28,29 +32,44 @@ async fn main() {
     );
 
     let p3 = Planet::new(
-        2.0,
+        1.0,
         Vec3::new(width / 2.0, height / 2.0 + 100.0, 0.0),
-        Vec3::new(10.0, -1.0, 0.0),
+        Vec3::new(30.0, -1.0, 0.0),
     );
     // init planets
     let mut planets = vec![p1, p2, p3];
 
     // init mouse status
     let mut button_down_flag = false;
+    let mut is_planet_num_max = false;
     let mut mouse_before = Vec3::new(0.0, 0.0, 0.0);
     loop {
-        clear_background(color_u8!(20, 20, 50, 1));
+        clear_background(color_u8!(20, 20, 50, 255));
+        if is_planet_num_max {
+            draw_text(
+                "Planet number is max. You can't add any more.",
+                50.0,
+                50.0,
+                30.0,
+                color_u8!(255, 100, 100, 255),
+            )
+        }
 
         // check mouse control
         let (x, y) = mouse_position();
+
         if is_mouse_button_down(MouseButton::Left) {
-            button_down_flag = true;
-            mouse_before = Vec3::new(x as f64, y as f64, 0.0);
+            if planets.len() >= PLANETS_MAX_NUM {
+                is_planet_num_max = true;
+            } else {
+                button_down_flag = true;
+                mouse_before = Vec3::new(x as f64, y as f64, 0.0);
+            }
         } else if button_down_flag {
             // add new planet when mouse up
             let p_pos = Vec3::new(x as f64, y as f64, 0.0);
             let p_vel = &p_pos - &mouse_before;
-            let p_new = Planet::new(5.0, p_pos, p_vel);
+            let p_new = Planet::new(2.0, p_pos, p_vel);
 
             planets.push(p_new);
             button_down_flag = false;
@@ -79,14 +98,13 @@ fn update_planets(planets: &mut Vec<Planet>) {
 }
 
 fn draw_planet(p: &Planet) {
-    let color = color_u8!(255, 150, 50, 255);
+    let color = color_u8!(250, 225, 100, 255);
     let r = (1000.0 * p.m).powf(0.33);
-    draw_circle(p.position.x as f32, p.position.y as f32, r as f32, color);
     let color_history = Color::new(color.r, color.g, color.b, 0.3);
     let calc_r = |i: usize| 2.0 * (500.0 - i as f32) / 500.0;
-
     p.position_history
         .iter()
         .enumerate()
-        .for_each(|(i, pos)| draw_circle(pos.x as f32, pos.y as f32, calc_r(i), color_history))
+        .for_each(|(i, pos)| draw_circle(pos.x as f32, pos.y as f32, calc_r(i), color_history));
+    draw_circle(p.position.x as f32, p.position.y as f32, r as f32, color);
 }
